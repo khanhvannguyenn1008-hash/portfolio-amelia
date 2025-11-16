14f2b58003631efb556188ceaa9efe471a510de2
Amelia <khanhvannguyenn1008@gmail.com>
2025-11-16 23:12:58 +0800

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  server: {
+    hmr: {
+      overlay: false,  
+    },
+  },
+
+  build: {
+    outDir: 'dist',  
+  },
+});
